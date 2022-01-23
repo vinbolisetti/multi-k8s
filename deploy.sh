b6afdbd6b442dff746b2764be2479e9fea0600eb
vinbolisetti <vinbolisetti@gmail.com>
2022-01-23 21:41:04 +0530

    first final commit 5

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t iovin/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t iovin/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t iovin/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t iovin/multi-client-k8s:latest -t iovin/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t iovin/multi-server-k8s-pgfix:latest -t iovin/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t iovin/multi-worker-k8s:latest -t iovin/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
 docker push iovin/multi-client-k8s:latest
 docker push iovin/multi-server-k8s-pgfix:latest
