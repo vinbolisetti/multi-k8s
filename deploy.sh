9779b6e8b0b574f1b7be00c5ab3e596664d1cb52
vinbolisetti <vinbolisetti@gmail.com>
2022-01-23 21:04:03 +0530

    first final commit 3

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-docker build -t vinbolisetti/multi-client:latest -t vinbolisetti/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t vinbolisetti/multi-server:latest -t vinbolisetti/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t vinbolisetti/multi-worker:latest -t vinbolisetti/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push vinbolisetti/multi-client:latest
-docker push vinbolisetti/multi-server:latest
-docker push vinbolisetti/multi-worker:latest
-docker push vinbolisetti/multi-client:$SHA
-docker push vinbolisetti/multi-server:$SHA
-docker push vinbolisetti/multi-worker:$SHA
+docker build -t vinbolisetti/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t vinbolisetti/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t vinbolisetti/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+
+docker push vinbolisetti/multi-client-k8s:latest
+docker push vinbolisetti/multi-server-k8s-pgfix:latest
+docker push vinbolisetti/multi-worker-k8s:latest
+
+docker push vinbolisetti/multi-client-k8s:$SHA
+docker push vinbolisetti/multi-server-k8s-pgfix:$SHA
+docker push vinbolisetti/multi-worker-k8s:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=vinbolisetti/multi-server:$SHA
-kubectl set image deployments/client-deployment server=vinbolisetti/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=vinbolisetti/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=vinbolisetti/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=vinbolisetti/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=vinbolisetti/multi-worker-k8s:$SHA
